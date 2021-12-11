efed0213ab87350e6ae1743c36d2a98d065929ec
Virtblch <94992045+Virtblch@users.noreply.github.com>
2021-12-11 20:53:17 +0300

    SasDate to Date and SasDateTime to DateTime in one activity

--- app/src/main/java/com/example/calcdatesas/MainActivity.kt
+++ app/src/main/java/com/example/calcdatesas/MainActivity.kt
@@ -10,23 +10,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val button: Button =findViewById(R.id.bt1)
-        button.setOnClickListener {
-            intent = Intent(this, SAStoDate::class.java)
-            startActivity(intent)
-        }
-
         val button2: Button =findViewById(R.id.bt2)
         button2.setOnClickListener {
             intent = Intent(this, SAStoTime::class.java)
             startActivity(intent)
         }
-
- /*       val button3: Button =findViewById(R.id.bt3)
-        button3.setOnClickListener {
-            intent = Intent(this, DateToSAS::class.java)
-            startActivity(intent)
-        }*/
 
         val button4: Button =findViewById(R.id.bt4)
         button4.setOnClickListener {
--- app/src/main/java/com/example/calcdatesas/SAStoTime.kt
+++ app/src/main/java/com/example/calcdatesas/SAStoTime.kt
@@ -6,9 +6,10 @@ import android.text.Editable
 import android.text.TextWatcher
 import android.widget.EditText
 import android.widget.TextView
-import java.text.SimpleDateFormat
-import java.util.*
-import java.util.concurrent.TimeUnit
+import java.time.Instant
+import java.time.ZoneId
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
 
 class SAStoTime : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,11 +18,10 @@ class SAStoTime : AppCompatActivity() {
 
 
         val tv1: TextView = findViewById(R.id.tv1)
-        val lbl: TextView = findViewById(R.id.lbl)
         val edt1: EditText = findViewById(R.id.edt1)
-
-        val tz = TimeZone.getDefault().id
-        lbl.text="Date and Time $tz:"
+        val tvD: TextView = findViewById(R.id.tvD)
+        val edtD: EditText = findViewById(R.id.edtD)
+        val utc0Zone = ZoneId.of("UTC+0")
 
         edt1.addTextChangedListener(object : TextWatcher {
 
@@ -35,24 +35,46 @@ class SAStoTime : AppCompatActivity() {
                                        before: Int, count: Int) {
                 try {
                     var sasDate: Long = s.toString().toLong()
-                    var pMils: Long
-                    var formatter = SimpleDateFormat("yyyy-MM-dd HH:mm:ss") // modify format
+                    val i: Instant = Instant.ofEpochMilli(sasDate*1000 - 315619200000)
+                    val aDateTime: ZonedDateTime = ZonedDateTime.ofInstant(i, utc0Zone)
 
-
-                    if (sasDate<0 && sasDate>=-11897020800){
-                        pMils = -(TimeUnit.SECONDS.toMillis(Math.abs(sasDate)) + 315619200000)
-                        tv1.text = formatter.format(Date(pMils))
-                    }
-                    else if(sasDate>=0 && sasDate<=566131680000){
-                        pMils= TimeUnit.SECONDS.toMillis(sasDate) - 315619200000
-                        tv1.text = formatter.format(Date(pMils))
+                    var formatter: DateTimeFormatter = DateTimeFormatter.ofPattern("dd MMM yyyy HH:mm:ss z")
+                    val formattedString: String = aDateTime.format(formatter)
+                    if (sasDate>=-11928470400 && sasDate<=566131766399) {
+                        tv1.text = formattedString
                     }
                     else{
-                        tv1.text = "out of range"
+                        tv1.text = "SAS DateTime out of range"
                     }
-                    //var formatter = SimpleDateFormat1("yyyy-MM-dd HH:mm:ss") // modify format
-
                 } catch (e: Exception) {tv1.text=""}
+            }
+        })
+
+        edtD.addTextChangedListener(object : TextWatcher {
+
+            override fun afterTextChanged(s: Editable) {}
+
+            override fun beforeTextChanged(s: CharSequence, start: Int,
+                                           count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence, start: Int,
+                                       before: Int, count: Int) {
+                try {
+                    var sasDate: Long = s.toString().toLong()
+                    sasDate=sasDate*24*60*60
+                    val i: Instant = Instant.ofEpochMilli(sasDate*1000 - 315619200000)
+                    val aDateTime: ZonedDateTime = ZonedDateTime.ofInstant(i, utc0Zone)
+
+                    var formatter: DateTimeFormatter = DateTimeFormatter.ofPattern("dd MMM yyyy")
+                    val formattedString: String = aDateTime.format(formatter)
+                    if (sasDate>=-11928470400 && sasDate<=566131766399) {
+                        tvD.text = formattedString
+                    }
+                    else{
+                        tvD.text = "SAS Date out of range"
+                    }
+                } catch (e: Exception) {tvD.text=""}
             }
         })
 
